c592c94406502b2c74398198ccf5354f7c982e96
hitchless <jonny@hitchless.com>
2023-11-14 23:11:16 -0800

    start building out first iteration of site ... aka basic scaffolding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const colors = require('tailwindcss/colors');
+module.exports = {
 	content: [
-			'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-			'./node_modules/flowbite/**/*.js',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'tuepah': "url('/public/tuepah-front.jpg')",
+			}
+		},
+		colors: {
+			primary: colors.sky,
+			secondary: colors.emerald,
+			base: colors.slate,
+		},
 	},
 	plugins: [
 		require('flowbite/plugin'),
